bcc4b6c6c778ba93181031b883e0ec71cbf80b71
xXJoaoVictorNogXx <joaovictorfcantanheden@gmail.com>
2026-08-28 16:19:19 -0300

    Add Vercel configuration for API rewrites; update Vite proxy settings for product API

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   server: {
     proxy: {
-      "/api": {
+      "/api/produtos": {
         target: "https://app.econverse.com.br",
         changeOrigin: true,
-        secure: false,
+        rewrite: (path) =>
+          path.replace(
+            /^\/api\/produtos/,
+            "/teste-front-end/junior/tecnologia/lista-produtos/produtos.json",
+          ),
       },
     },
   },
